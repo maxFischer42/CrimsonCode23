68100c4e4114f5df34211cd2595f79f136475cf3
Max Fischer <maxfischer072@gmail.com>
2023-02-19 07:56:39 -0800

    Updated default business page

--- ClientApp/src/components/BusinessPage.js
+++ ClientApp/src/components/BusinessPage.js
@@ -23,9 +23,9 @@ export class BusinessPage extends Component {
         return(
             <div>
                 <Return path={'/explore'}/>
-                <ProfileHeader name="Test Name" banner="https://images.unsplash.com/photo-1597251147933-2473060caa68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHJ1YmJlciUyMGR1Y2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                address="123 Duck Blvd." tier="$$$" hours={this.convertToTimeRange([9, 23])}/>
-                <ProfileCard title="About" data="Quack"/>
+                <ProfileHeader name="Sugar Squares" banner="https://images.unsplash.com/photo-1459789034005-ba29c5783491?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=938&q=80"
+                address="223 Belgium Ave." tier="$$$" hours={this.convertToTimeRange([7, 19])}/>
+                <ProfileCard title="About" data="Waffle-oriented restaraunt; established in 1607."/>
                 <Claim/>
             </div>
         );
