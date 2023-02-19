2795a9bd8e0c97fa72cb17db23c1d5f7b7fc93b6
u1F98E <u1f98e@proton.me>
2023-02-19 07:31:42 -0800

    Added price_tier

--- src/FeedCard.cs
+++ src/FeedCard.cs
@@ -8,12 +8,13 @@ public class FeedCard
     public String? banner { get; set; }
     public String? logo { get; set; }
     public String? categoryId { get; set; }
-    public String? price_tier { get; set; }
+    public int price_tier { get; set; }
 
 	static public FeedCard FromSqlReader(SqliteDataReader reader) => new FeedCard {
 		id          = Util.GetOrDefault<int>(reader, "id", 0),
 		name        = Util.GetOrDefault<String>(reader, "name", ""),
 		banner      = Util.GetOrDefault<String>(reader, "bannerURL", ""),
+		price_tier  = Util.GetOrDefault<int>(reader, "priceTier", -1),
 		categoryId  = Util.GetOrDefault<String>(reader, "categoryId", "")
 	};
 }
