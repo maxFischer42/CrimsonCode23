df1847e9e2038687963afc433f66dcfa60d6acfe
u1F98E <u1f98e@proton.me>
2023-02-19 00:04:38 -0800

    Pushed other stuff

--- ClientApp/src/components/CategoryExplorer.js
+++ ClientApp/src/components/CategoryExplorer.js
@@ -9,31 +9,39 @@ import { CategoryCard } from './CategoryCard';
 export class CategoryExplorer extends Component {
   static displayName = CategoryExplorer.name;
   
+  state = { data: null }
+  
   constructor (props) {
     super(props);
 
     // Here we will need to call to the server to request all locations with the given tag
     // For now though, we will use "testingData.js" to get our testing data
-
-
-    
   }
 
+  componentDidMount() {
+    fetch('api/get_cards_from_category/' + this.props.category)
+      .then(resp => resp.json())
+      .then(data => this.setState({data}));
+  }
 
-  /*getCategoryCards() {
-    let data = [];
-    let name = ["Food", "Hobbies", "Entertainment", "Groceries", "Sports", "Music", "Hair", "Clothing"]
-    for(let i = 0; i < name.length; i++) {
-        data[i] = (<CategoryCard name={name[i]}/>);
-    }
-    return data;
-  }*/
+  getCards() {
+    return (this.state.data.map(business => {
+        return (<BusinessCard key={business.id} name={business.name} id={business.id}/>);
+    }));
+  }
 
   render() {
-    return (
-      <div>
-        <label>{this.props.category}</label>
-      </div>
-    );
+    if(this.state.data === null) { 
+      return (<p>Loading...</p>);
+    }
+    else {
+      return (
+        <div>
+          <Row style={{overflow: 'auto'}}>
+              {this.getCards()}
+          </Row>
+        </div>
+      );
+    }
   }
 }
